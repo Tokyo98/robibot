1ee4d3dd1706495c823794278d7ae3d8d7672a8e
Tokyo98 <70757938+Tokyo98@users.noreply.github.com>
2020-11-28 22:55:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-	"TOKEN": process.env.TOKEN,
+	"TOKEN":"NzgyMzMwNDc5MTEwMTI3NjI2.X8KoCQ.zcsz0nDnqoLYqObA4J0YvFmBQ-I"
 	"YOUTUBE_API_KEY": process.env.YOUTUBE_API_KEY,
 	"MAX_PLAYLIST_SIZE": 10,
 	"PREFIX": "!",
